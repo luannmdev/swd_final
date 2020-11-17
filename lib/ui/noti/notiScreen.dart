6364f558077047c34861a538f6c5c9a19151702e
thuan <thuanhoang2108@gmail.com>
2020-11-18 02:29:10 +0700

    sửa trang news, thêm trang noti trắng

---
+++ lib/ui/noti/notiScreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class NotiPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return NotiState();
+  }
+  
+}
+
+class NotiState extends State<NotiPage>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold();
+  }
+  
+}
